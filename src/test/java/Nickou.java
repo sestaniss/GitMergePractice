3cb778be7ca9bf3744a2c312684cacd4002ed4c9
sestaniss <sam.estaniss@gmail.com>
2023-04-10 21:59:15 -0500

    Nickou is sad because I love Tami more

--- src/test/java/Nickou.java
+++ src/test/java/Nickou.java
@@ -1,5 +1,6 @@
 public class Nickou {
     public static void main(String[] args) {
-        System.out.println("I Love Tami more than Nickou");
+
+        System.out.println("I Love Tami more than Nickou and Nickou gets jealous");
     }
 }
